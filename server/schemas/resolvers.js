f4e334d8c8b5ad3af99e8a04d823d82a539f8017
zue <zuleikatesei@gmail.com>
2022-11-17 16:33:30 -0800

    front end not showing up on port 3000

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,34 +4,37 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async (parent, args) => {
-            const userData = await User.findOne({})
-                .select('-__v -password')
-                .populate('thoughts')
-                .populate('friends');
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
 
-            return userData;
-        },
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Thought.find().sort({ createdAt: -1 });
-        },
-        thought: async (parent, { _id }) => {
-            return Thought.findOne({ _id });
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
         },
         users: async () => {
             return User.find()
                 .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
+                .populate('thoughts')
+                .populate('friends');
         },
         user: async (parent, { username }) => {
             return User.findOne({ username })
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        },
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
         }
-
     },
     Mutation: {
         addUser: async (parent, args) => {
