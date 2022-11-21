f677f5f512815a8da45b920fbb75e71812de3ec8
zue <zuleikatesei@gmail.com>
2022-11-20 16:25:01 -0800

    reaction form

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -32,6 +32,8 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+
     </div>
   );
 };
